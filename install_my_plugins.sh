e9c597f549f975387a80e5bd938dd77484aa28da
Tiffany Lin <tifflin@uber.com>
2020-08-06 14:43:26 -0700

    Add git gutter plugin

--- install_my_plugins.sh
+++ install_my_plugins.sh
@@ -1,2 +1,4 @@
 git clone https://github.com/fatih/vim-go ./my_plugins/vim-go
 git clone https://github.com/NLKNguyen/papercolor-theme.git ./my_plugins/papercolor-theme
+git clone https://github.com/airblade/vim-gitgutter.git ./my_plugins/vim-gitgutter
+
