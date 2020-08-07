686d35e967374acdaee47975e404021935e69874
Tiffany Lin <tifflin@uber.com>
2020-08-07 11:08:15 -0700

    Remove gitgutter (already included)

--- install_my_plugins.sh
+++ install_my_plugins.sh
@@ -1,4 +1,3 @@
 git clone https://github.com/fatih/vim-go ./my_plugins/vim-go
 git clone https://github.com/NLKNguyen/papercolor-theme.git ./my_plugins/papercolor-theme
-git clone https://github.com/airblade/vim-gitgutter.git ./my_plugins/vim-gitgutter
 
