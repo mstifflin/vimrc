2a09f6ddd196a02a1d7553f9363ebfa0edaccb03
Tiffany Lin <me@tifflin.dev>
2020-08-10 10:33:13 -0700

    Add YouCompleteMe plugin

--- install_my_plugins.sh
+++ install_my_plugins.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/fatih/vim-go ./my_plugins/vim-go
 git clone https://github.com/NLKNguyen/papercolor-theme.git ./my_plugins/papercolor-theme
+git clone https://github.com/ycm-core/YouCompleteMe.git ./my_plugins/youcompleteme
+
 
