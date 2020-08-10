bf93ccaced52dcd2d1e6e501a0611639abddc191
Tiffany Lin <tifflin@uber.com>
2020-08-10 14:35:58 -0700

    Revert "Add YouCompleteMe plugin"
    This reverts commit 2a09f6ddd196a02a1d7553f9363ebfa0edaccb03.

--- install_my_plugins.sh
+++ install_my_plugins.sh
@@ -1,5 +1,3 @@
 git clone https://github.com/fatih/vim-go ./my_plugins/vim-go
 git clone https://github.com/NLKNguyen/papercolor-theme.git ./my_plugins/papercolor-theme
-git clone https://github.com/ycm-core/YouCompleteMe.git ./my_plugins/youcompleteme
-
 
