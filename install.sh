7713d156c2069de335f9bce684bc839bf4997b16
Tiffany Lin <me@tifflin.dev>
2021-05-30 11:58:29 -0700

    Add neovim init set up to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-~/.vim_runtime/install_my_plugins.sh
+# Only needed for vim? Not needed for neovim, I think
+# ~/.vim_runtime/install_deoplate_dependencies.sh
 ~/.vim_runtime/install_awesome_vimrc.sh
+mkdir -p ~/.config/nvim
 
+if [ ! -e ~/.config/nvim/init.vim ]; then
+  ln -s ~/.vim_runtime/nvim_init.vim ~/.config/nvim/init.vim
+fi
 
