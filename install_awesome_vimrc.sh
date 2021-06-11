50b6684c02243f71fe3ffa8f762bf2c942a36217
Tiffany Lin <tifflin@uber.com>
2021-06-11 17:51:50 +0000

    Add notes on fixing npm registry, move coc config to separate file

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -8,6 +8,7 @@ echo 'set runtimepath+=~/.vim_runtime
 source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
+source ~/.vim_runtime/vimrcs/coc_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
 
 try
